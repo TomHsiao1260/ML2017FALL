d4dfdfb703f99a7f1d36cf10cc524736a93e9012
Tom Hsiao <tomhsiao>
2017-09-16 07:01:04 +0800

    modify

--- hw0/Q2.py
+++ hw0/Q2.py
@@ -2,11 +2,11 @@ from PIL import Image
 import sys
 path = sys.argv[1]
 
-with open(path) as inputimage:
-    pixel = inputimage.load()
+image = Image.open(path)
+pixel = image.load()
 
-    for i in range(inputimage.size[0]):
-        for j in range(inputimage.size[1]):
-            pixel[i,j] = (pixel[i,j][0]//2,pixel[i,j][1]//2,pixel[i,j][2]//2)
-            
-    inputimage.save("Q2.png")
+for i in range(image.size[0]):
+    for j in range(image.size[1]):
+        pixel[i,j] = (pixel[i,j][0]//2,pixel[i,j][1]//2,pixel[i,j][2]//2)
+        
+image.save("Q2.png")
